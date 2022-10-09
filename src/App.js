bd6b3e911124ea1b9fe79e30477f3d9e75beb68e
Sherifjustice <sherifjustice7@gmail.com>
2022-10-09 20:30:32 +0300

    narabotki

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Users from './components/users'
 import SearchStatus from './components/searchStatus'
 import api from './api'
 import Pagination from './components/pagination'
 import { paginate } from './utils/paginate'
+import GroupList from './components/groupList'
 
 function App() {
 	const [users, setUsers] = useState(api.users.fetchAll())
+	const [currentPage, setCurrentPage] = useState(1)
+	const [professions, setProfessions] = useState(api.professions.fetchAll())
+	const [selectedProf, setSelectedProf] = useState()
+
+	const pageSize = 4
+	const filteredUsers = selectedProf
+		? users.filter(
+				(user) =>
+					JSON.stringify(user.profession) === JSON.stringify(selectedProf) // из чата
+		  )
+		: users
+	const count = filteredUsers.length
+	const userCrop = paginate(filteredUsers, currentPage, pageSize)
 	const handleDelete = (userId) => {
-		setUsers((prevState) => prevState.filter((user) => user._id !== userId))
+		setUsers(users.filter((user) => user._id !== userId))
 	}
 	const handleToggleBookMark = (id) => {
 		setUsers(
@@ -21,43 +35,69 @@ function App() {
 			})
 		)
 	}
-	const count = users.length
-	const pageSize = 4
-	const [currentPage, setCurrentPage] = useState(1)
 	const handlePageChange = (pageIndex) => {
 		setCurrentPage(pageIndex)
 	}
-	const userCrop = paginate(users, currentPage, pageSize)
-
+	const handleProfessionSelect = (item) => {
+		setSelectedProf(item)
+	}
+	const clearFilter = () => {
+		setSelectedProf()
+	}
+	useEffect(() => {
+		api.professions.fetchAll().then((data) => setProfessions(data))
+	}, [])
+	useEffect(() => {
+		api.users.fetchAll().then((data) => setUsers(data))
+	}, [])
+	useEffect(() => {
+		setCurrentPage(1)
+	}, [selectedProf])
 	return (
-		<div>
-			<SearchStatus length={users.length} />
-			<table className={count ? 'table' : 'hide'}>
-				<thead>
-					<tr>
-						<th scope="col">Имя</th>
-						<th scope="col">Качества</th>
-						<th scope="col">Профессия</th>
-						<th scope="col">Встретился, раз</th>
-						<th scope="col">Оценка</th>
-						<th scope="col">Избранное</th>
-						<th></th>
-					</tr>
-				</thead>
-				<tbody>
-					<Users
-						users={userCrop}
-						handleDelete={handleDelete}
-						handleToggleBookMark={handleToggleBookMark}
+		<div className="d-flex">
+			{professions && (
+				<div className="d-flex flex-column flex-shrink-0 p-3">
+					<GroupList
+						selectedItem={selectedProf}
+						items={professions}
+						onItemSelect={handleProfessionSelect}
 					/>
-				</tbody>
-			</table>
-			<Pagination
-				itemsCount={count}
-				pageSize={pageSize}
-				currentPage={currentPage}
-				onPageChange={handlePageChange}
-			/>
+					<button className="btn btn-secondary mt-2" onClick={clearFilter}>
+						Очистить
+					</button>
+				</div>
+			)}
+			<div className="d-flex flex-column">
+				<SearchStatus length={count} />
+				<table className={count ? 'table' : 'hide'}>
+					<thead>
+						<tr>
+							<th scope="col">Имя</th>
+							<th scope="col">Качества</th>
+							<th scope="col">Профессия</th>
+							<th scope="col">Встретился, раз</th>
+							<th scope="col">Оценка</th>
+							<th scope="col">Избранное</th>
+							<th></th>
+						</tr>
+					</thead>
+					<tbody>
+						<Users
+							users={userCrop}
+							handleDelete={handleDelete}
+							handleToggleBookMark={handleToggleBookMark}
+						/>
+					</tbody>
+				</table>
+				<div className="d-flex justify-content-center">
+					<Pagination
+						itemsCount={count}
+						pageSize={pageSize}
+						currentPage={currentPage}
+						onPageChange={handlePageChange}
+					/>
+				</div>
+			</div>
 		</div>
 	)
 }
